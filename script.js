94fa3f14f5f113d9ab446a9f58a4494fb5ca6b94
PriyanshuRawat24 <rawatpriyanshu653@gmail.com>
2024-10-10 16:37:50 +0530

    update new page

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    if ($(window).width() > 991){
+    $('.navbar-light .d-menu').hover(function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+        }, function () {
+            $(this).find('.sm-menu').first().stop(true, true).delay(120).slideUp(100);
+        });
+        }
+    });
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:7
+            },
+            1000:{
+                items:7
+            }
+        }
+    });
