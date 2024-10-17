4ef1a6aecdfe30e8d898bb781c25f438cc1ba2f6
PriyanshuRawat24 <rawatpriyanshu653@gmail.com>
2024-10-17 16:28:35 +0530

    update

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-$(document).ready(function () {
-    if ($(window).width() > 991){
-    $('.navbar-light .d-menu').hover(function () {
-            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
-        }, function () {
-            $(this).find('.sm-menu').first().stop(true, true).delay(120).slideUp(100);
-        });
-        }
-    });
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
 
-
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        autoplay:true,
-        autoplayHoverPause: true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:3
-            }
-        }
-    });
-
-  AOS.init();
+AOS.init();
